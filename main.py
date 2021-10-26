4f4249784a7bc9b5976263f8c4ce0bee1fbee286
Tonya Oliver <tonya.anne.oliver@gmail.com>
2021-10-26 13:58:41 -0400

    adding function feature

--- main.py
+++ main.py
@@ -1,50 +1,88 @@
 from random import randint
-#add player and computer lives
-playerLives = 5
-computerLives = 5
+
+# add player and computer lives
+choices = ["rock", "paper", "scissors"]
+
 
 # save the player as a varible called player
 # the value of player will be one of three choices to type (input)
-player = input("Choose rock, paper or scissors")
-
-print("player chose: " + player)
+#
+# player will be the weapon the player chooses via input
+player = False
 
 # and array is just a container. It holds multiple values in a 0-based index
 # you can store anything in an arra(y and retrieve it later. Arrays have a square bracket notation
+#Boolean values are True or False - you can use these to check state
 
-choices = ["rock", "paper", "scissors"]
+playerLives = 5
+computerLives = 5
+# define a win / lose function and invoke it in our game loop when lives run out (player or computer)
 
-computer = choices[randint(0,2)]
+# create an infinite loop (for now) so that we can keep playing
 
-print("computer chose: " + computer)
 
-if (computer == player):
-	print("tie! try again")
+def winorlose(status):
+	print("You " + status + "! Would you like to play again?")
+	choice = input(" Y / N ")
 
-elif (player == "rock"):
-	if (computer == "paper"):
-		print("you lose!")
-		playerLives = playerLives - 1
-	else:
-		print("you win!")
-		computerLives = computerLives -1
+	global playerLives
+	global computerLives
+	global player
 
-elif (player == "scissors"):
-	if (computer == "paper"):
-		print("You win!")
-		computerLives = computerLives -1
-	else:
-		print("You lose!")
-		playerLives = playerLives - 1
+		if choice =="n":
+			print("Better luck next time!")
+			exit()
+		else:
+			#reset and restart
+			playerlives= 5
+			computerLives= 5
+			player = False
 
-elif (player == "paper"):
-	if (computer == "rock"):
-		print("You win!")
-		computerLives = computerLives -1
-	else:
-		print("You lose!")
-		playerLives = playerLives - 1
 
-print("computer Lives: " + str(computerLives))
-print("player Lives: " + str(playerLives))
+while player is False:
 
+	player = input("Choose your weapon! rock, paper or scissors:")
+	computer = choices[randint(0,2)]
+
+
+	print("computer chose: " + computer)
+	print("player chose: " + player)
+
+	if (computer == player):
+		print("tie! try again")
+
+	elif (player == "rock"):
+		if (computer == "paper"):
+			print("you lose!")
+			playerLives = playerLives - 1
+		else:
+			print("you win!")
+			computerLives = computerLives -1
+
+	elif (player == "scissors"):
+		if (computer == "paper"):
+			print("You win!")
+			computerLives = computerLives -1
+		else:
+			print("You lose!")
+			playerLives = playerLives - 1
+
+	elif (player == "paper"):
+		if (computer == "rock"):
+			print("You win!")
+			computerLives = computerLives -1
+		else:
+			print("You lose!")
+			playerLives = playerLives - 1
+
+	print("computer Life count: " + str(computerLives))
+	print("player Life count: " + str(playerLives))
+
+
+	if playerLives ==0
+		winorlose("lost")
+
+	elif computerLives == 0:
+		winorlose("won")
+
+	player = False
